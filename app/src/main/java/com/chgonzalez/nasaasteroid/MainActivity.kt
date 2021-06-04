b05901232b02269d04adb759536069b02441a0af
Christian <thech999@gmail.com>
2021-06-04 12:46:40 +0100

    WorkManager

--- app/src/main/java/com/chgonzalez/nasaasteroid/MainActivity.kt
+++ app/src/main/java/com/chgonzalez/nasaasteroid/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.chgonzalez.nasaasteroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
+import com.chgonzalez.nasaasteroid.work.DataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    val applicationScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        delayedInit()
     }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<DataWork>(1, TimeUnit.DAYS)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            DataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+
 }
--- app/src/main/java/com/chgonzalez/nasaasteroid/work/DataWork.kt
+++ app/src/main/java/com/chgonzalez/nasaasteroid/work/DataWork.kt
@@ -1,2 +1,28 @@
 package com.chgonzalez.nasaasteroid.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.chgonzalez.nasaasteroid.database.getDatabase
+import com.chgonzalez.nasaasteroid.repository.AsteroidRepository
+import retrofit2.HttpException
+
+class DataWork(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "DataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+
+        val repository = AsteroidRepository(getDatabase(applicationContext))
+
+        return try {
+            repository.refreshAsteroids()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
