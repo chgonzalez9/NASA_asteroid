7fd3c9b5bb007a03171037476bc119e48a07f043
Christian <thech999@gmail.com>
2021-06-06 21:26:11 +0100

    Hazardous description

--- app/src/main/java/com/chgonzalez/nasaasteroid/util/BindingAdapters.kt
+++ app/src/main/java/com/chgonzalez/nasaasteroid/util/BindingAdapters.kt
@@ -42,8 +42,10 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription = imageView.context.getString(R.string.isHazardous)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription = imageView.context.getString(R.string.isSafe)
     }
 }
 
